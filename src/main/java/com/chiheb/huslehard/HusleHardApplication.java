807b399d627a1c9986ea446fac39b106982d9981
Chihebkebbas <kebbas.chihebeddine1@gmail.com>
2026-01-10 16:36:00 +0100

    Initialize project backend

---
+++ src/main/java/com/chiheb/huslehard/HusleHardApplication.java
@@ -0,0 +1,13 @@
+package com.chiheb.huslehard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HusleHardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HusleHardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chiheb/huslehard/HusleHardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiheb.huslehard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HusleHardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
